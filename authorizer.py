d3725296723e2b3527ac097f84f735c7733e5720
Franky Martin <fmartin@neutral.io>
2016-12-06 11:39:40 -0500

    PROD-64, secure task refactoring

--- authorizer.py
+++ authorizer.py
@@ -17,12 +17,12 @@ class Authorizer(object):
         if not isinstance(user, User) or not isinstance(task, SecureTask):
             raise Unauthorized()
 
-        my_permission = task.task
         for perm_def in cls._get_permissions_for_user(user.name):
             # See if the permission we are checking is in the user's
-            # permisssion set
-            if cls._permission_matches_permission_def(my_permission, perm_def):
-                # The permission matches, return indidcating they are
+            # permission set
+            if cls._permission_matches_permission_def(
+                    task.resource, task.permission, perm_def):
+                # The permission matches, return indicating they are
                 # authorized
                 return
 
@@ -33,23 +33,23 @@ class Authorizer(object):
     def _get_permissions_for_user(cls, username):
         """ Function to return a list of permissions for a user.
 
-        This could be extended to lookup user roles and gather permisions
+        This could be extended to lookup user roles and gather permissions
         from the role in addition to the specified permissions.
         """
         return cls._permissions.get(username, [])
 
     @classmethod
-    def _permission_matches_permission_def(cls, perm_to_check, perm_def):
+    def _permission_matches_permission_def(cls, resource, permission, perm_def):
         """ Checks that a permission matches a permission definition
 
         The permission definition is the one that should come from the config
         and can have certain rules (like * wildcards)
 
         Example:
-            >>> _permission_matches_permission_def("a.b", "a.*") == True
-            >>> _permission_matches_permission_def("a.b", "a.b") == True
-            >>> _permission_matches_permission_def("a.b", "a") == False
+            >> _permission_matches_permission_def("a.b", "a.*") == True
+            >> _permission_matches_permission_def("a.b", "a.b") == True
+            >> _permission_matches_permission_def("a.b", "a") == False
         """
         # Going to hack this by using fnmatch which matches filenames
         # https://docs.python.org/3.5/library/fnmatch.html
-        return fnmatch(perm_to_check, perm_def)
+        return fnmatch("{}.{}".format(resource, permission), perm_def)
